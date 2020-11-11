d526346822da4da9f09fa056b92d8a5e49140dda
HILTON731 <hilton3180@gmail.com>
2020-11-11 18:02:16 +0900

    with file

--- src/dke/algorithm/assignment/seqmatmul/SeqMatMul.java
+++ src/dke/algorithm/assignment/seqmatmul/SeqMatMul.java
@@ -1,28 +1,50 @@
 package dke.algorithm.assignment.seqmatmul;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class SeqMatMul {
-    static int num;
 
-    public static void main(String[] args) {
-        int[][] d = creatMatrix();
-//        int[][] d = {{5, 0}, {2, 0}, {3, 0}, {4, 0}, {6, 0}, {7, 0}, {8, 0}};
-        System.out.println();
-        System.out.println(Arrays.deepToString(d));
-//        num = 7;
+    public static void main(String[] args) throws IOException {
+        int[][] d;
+        long start, end;
+        double BF, DP;
         SeqMatMul seqMatMul = new SeqMatMul();
-        System.out.println(seqMatMul.bruteForce(d, num - 1));
-        System.out.println(seqMatMul.dynamicProgramming(d,num - 1));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Matrix count num: ");
+        int num = sc.nextInt();
+        String createfile = "D:\\IdeaProjects\\Java_study\\seqmatmul.csv";
+        FileWriter fw = new FileWriter(createfile);
+        fw.append("SIZE").append(',').append("BF_TIME").append(',').append("DP_TIME").append('\n');
+
+
+        for (int i = 3; i <= num; i++) {
+            d = creatMatrix(i + 1);
+            System.out.println();
+            System.out.println(Arrays.deepToString(d));
+
+            System.out.print("bruteForce result: ");
+            start = System.nanoTime();
+            System.out.println(seqMatMul.bruteForce(d, i, num));
+            end = System.nanoTime();
+            System.out.println("bruteForce time: " + (BF = (end - start) / 1000000000.0));
+
+            System.out.print("dP result: ");
+            start = System.nanoTime();
+            System.out.println(seqMatMul.dynamicProgramming(d, i));
+            end = System.nanoTime();
+            System.out.println("dP time: " + (DP = (end - start) / 1000000000.0));
+            fw.append(Integer.toString(i)).append(',').append(Double.toString(BF)).append(',').append(Double.toString(DP)).append('\n');
+        }
+        fw.flush();
+        fw.close();
     }
 
-    public static int[][] creatMatrix() {
-        Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
-        System.out.print("Matrix count num: ");
-        num = sc.nextInt() + 1;
+    public static int[][] creatMatrix(int num) {
+        Random rand = new Random(777);
         int[][] d = new int[num][2];
         for (int i = 0; i < num; i++) {
             d[i][0] = rand.nextInt(10) + 1;
@@ -40,7 +62,7 @@ public class SeqMatMul {
         return count > 2;
     }
 
-    public int bruteForce(int[][] d, int count) {
+    public int bruteForce(int[][] d, int count, int num) {
         int[] fin = new int[count - 1];
         int tmp, temp = 0;
         for (int i = 0; i < count - 1 && countZero(d, i); i++) {
@@ -56,7 +78,7 @@ public class SeqMatMul {
                 }
             }
             if (count < 3) fin[i] = tmp;
-            else fin[i] = tmp + bruteForce(d, count - 1);
+            else fin[i] = tmp + bruteForce(d, count - 1, num);
             d[temp][1] = 0;
         }
         if (fin.length == 1) return fin[0];
@@ -68,18 +90,19 @@ public class SeqMatMul {
             return tmp;
         }
     }
-    public int dynamicProgramming(int[][] d, int num){
+
+    public int dynamicProgramming(int[][] d, int num) {
         int[][] M = new int[num][num];
         int[][] P = new int[num - 1][num];
-        int diagonal = 0;
-        int i, j, k = 0, temp, tmp = 0;
+        int diagonal;
+        int i, j, k, temp, tmp = 0;
 
-        for(diagonal = 1; diagonal < num;diagonal++){
-            for(i = 0; i < num-diagonal; i++){
+        for (diagonal = 1; diagonal < num; diagonal++) {
+            for (i = 0; i < num - diagonal; i++) {
                 j = i + diagonal;
                 temp = Integer.MAX_VALUE;
-                for(k = i; k < j;k++){
-                    if(temp > M[i][k] + M[k+1][j]+d[i][0] * d[k + 1][0] * d[j + 1][0]) {
+                for (k = i; k < j; k++) {
+                    if (temp > M[i][k] + M[k + 1][j] + d[i][0] * d[k + 1][0] * d[j + 1][0]) {
                         temp = M[i][k] + M[k + 1][j] + d[i][0] * d[k + 1][0] * d[j + 1][0];
                         tmp = k;
                     }
@@ -87,16 +110,6 @@ public class SeqMatMul {
                 M[i][j] = temp;
                 P[i][j] = tmp + 1;
             }
-        }
-        System.out.println("M");
-        for(int[] cs : M){
-            for(int ct: cs)
-                System.out.print(ct+"\t");
-            System.out.println();
-        }
-        System.out.println("P");
-        for(int[] cs : P){
-            System.out.println(Arrays.toString(cs));
         }
         return M[0][num - 1];
     }
--- src/dke/algorithm/assignment/shortestpath/ShortestPath.java
+++ src/dke/algorithm/assignment/shortestpath/ShortestPath.java
@@ -2,91 +2,63 @@ package dke.algorithm.assignment.shortestpath;
 
 import edu.princeton.cs.introcs.In;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class ShortestPath {
     static int nan = 1000;
-    static int[][] W = {
-            {0, 1, nan, 1, 5},
-            {9, 0, 3, 2, nan},
-            {nan, nan, 0, 4, nan},
-            {nan, nan, 2, 0, 3},
-            {3, nan, nan, nan, 0}
-    };
+    static int[][] W;
     static int[][] D;
     static int[][] P;
-    public static void main(String[] args) {
+    static int[] F;
+    public static void main(String[] args) throws IOException {
         ShortestPath shortestPath = new ShortestPath();
         Scanner sc = new Scanner(System.in);
-        int num = 5;
+        int num;
+        long start, end;
+        double t1, t2;
+        String createfile = "D:\\IdeaProjects\\Java_study\\shortestpath.csv";
+        FileWriter fw = new FileWriter(createfile);
+        fw.append("SIZE").append(',').append("DP_TIME").append(',').append("dijkstra_TIME").append('\n');
 
-//        System.out.print("number of vertex: ");
-//        num = sc.nextInt();
-//        createPath(num);
-        shortestPath.dynamicProgramming(W, num);
-        System.out.println("W");
-        for(int[] cs: W){
-            for(int t: cs){
-                System.out.print(t+"\t");
-            }
-            System.out.println();
+        System.out.print("number of vertex: ");
+        num = sc.nextInt();
+        for(int i = 3; i <= num; i++) {
+            createPath(i);
+            start = System.nanoTime();
+            shortestPath.dynamicProgramming(W, i);
+            end = System.nanoTime();
+            System.out.println("DP result: " + Arrays.toString(D[0]) + " DP Time: " + (t1 = (end - start) / 1000000000.0));
+            start = System.nanoTime();
+            shortestPath.dijkstra(W, i);
+            end = System.nanoTime();
+            System.out.println("dijkstra result: " + Arrays.toString(F) + " dijkstra Time: " + (t2 = (end - start) / 1000000000.0));
+            fw.append(Integer.toString(i)).append(',').append(Double.toString(t1)).append(',').append(Double.toString(t2)).append('\n');
         }
-        System.out.println("D");
-        for(int[] cs: D){
-            for(int t: cs){
-                System.out.print(t+"\t");
-            }
-            System.out.println();
-        }
-        System.out.println("P");
-        for(int[] cs: P){
-            for(int t: cs){
-                System.out.print(t+"\t");
-            }
-            System.out.println();
-        }
-//        System.out.println("start, end point");
-//        int start = sc.nextInt(), end = sc.nextInt();
-//        shortestPath.path(start - 1, end - 1, P);
-
-        W = new int[][]{
-                {0, 1, 3, nan, nan},
-                {1, 0, 3, 6, nan},
-                {3, 3, 0, 4, 2},
-                {nan, 6, 4, 0, 5},
-                {nan, nan, 2, 5, 0}
-        };
-        shortestPath.dijkstra(W,num);
-
+        fw.flush();
+        fw.close();
     }
-    public static int[][] createPath(int size){
+    public static void createPath(int size){
         W = new int[size][size];
         Random random = new Random();
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
-                if(i!=j) {
-                    W[i][j] = (random.nextInt(9) + 1 < 1) ? Integer.MAX_VALUE : random.nextInt(10) + 1;
-                } else {
-                    W[i][j] = 0;
+                if(i<j){
+                    W[i][j] = W[j][i] = (random.nextInt(9) + 1 < 3) ? nan : random.nextInt(9) + 1;
                 }
             }
         }
-        return W;
     }
     public void dynamicProgramming(int[][] W, int n){
         D = new int[n][n];
         P = new int[n][n];
         int i, j, k;
-
         for(i = 0; i < n; i++){
             System.arraycopy(W[i], 0, D[i], 0, n);
         }
-        for(int[] cs: D){
-            System.out.println(Arrays.toString(cs));
-        }
-
         for(k = 0; k < n; k++){
             for(i = 0; i < n; i++){
                 for(j = 0; j < n; j++){
@@ -111,26 +83,21 @@ public class ShortestPath {
         int min, n = 0;
         int[] touch = new int[num];
         int[] length = new int[num];
+        F = new int[num];
 
         for(i = 1; i < num; i++){
             touch[i] = 0;
             length[i] = W[0][i];
         }
-        while(n < num){
-
-            System.out.println("step "+n);
-            System.out.println(Arrays.toString(length));
-            System.out.println(Arrays.toString(touch));
-
+        while(n < num - 1){
             min = Integer.MAX_VALUE;
             for(i = 1; i < num; i++){
                 if(0 <= length[i] && length[i] < min){
                     min = length[i];
                     vnear = i;
-                    System.out.println("sk"+vnear);
                 }
             }
-            System.out.println("vnear: "+vnear+" min: "+min);
+            F[vnear] = length[vnear];
             for(i = 1; i < num; i++){
                 if(length[vnear] + W[vnear][i] < length[i]){
                     length[i] = length[vnear] + W[vnear][i];
@@ -140,6 +107,5 @@ public class ShortestPath {
             length[vnear] = -1;
             n++;
         }
-
     }
 }
